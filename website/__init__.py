dadfb1bdf7592f0fb1c4f792ec9f372efc14bde6
jaydeep <jaydeeptalaviya8@gmail.com>
2022-03-24 07:19:41 +0530

    created model,form for login,register and initialized db

--- website/__init__.py
+++ website/__init__.py
@@ -1,10 +1,37 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from os import path
+db=SQLAlchemy()
 
 def createapp():
     #initializing app
     app=Flask(__name__)
+
+    #congigurations
+    app.config['SECRET_KEY']='Hello'
+    app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///database.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+
+    #DB settup
+    db.init_app(app)
+    loginmaneger=LoginManager(app)
+    loginmaneger.login_view='/'
+
+    # creating models
+    from .models import Users
+    createdatabse(app)
+    @loginmaneger.user_loader
+    def userloader(id):
+        return Users.query.filter_by(id=int(id)).first()
+
+
     #loading all the routes
     from .views import views
     app.register_blueprint(views,url_prefix='/')
     # return app
     return app
+
+def createdatabse(app):
+    if not path.exists('website/database.db'):
+        db.create_all(app=app)
--- website/views.py
+++ website/views.py
@@ -1,11 +1,67 @@
-from flask import Blueprint,render_template
-
+from flask import Blueprint,render_template,request,redirect,url_for
+from .models import Users
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import login_required,login_user,logout_user
+from . import db
 views=Blueprint('views',__name__)
 
 @views.route('/')
 def mainroute():
-    return '<h1>First Page</h1>'
+    return '<h1>abc</h1>'
 
-@views.route('/login')
+@views.route('/register',methods=['GET','POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        password2 = request.form.get('password2')
+
+        username_exiest=Users.query.filter_by(username=username).first()
+        email_exiest =Users.query.filter_by(email=email).first()
+
+        if password != password2:
+            print("Password Doesn't match")
+        elif username_exiest or email_exiest:
+            print("User already exiest")
+        elif len(password) <6 or len(username) <6:
+            print("Username or Password is too short")
+        else:
+            new_user=Users(username=username,email=email,password=generate_password_hash(password))
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user)
+            return redirect(url_for('views.posts'))
+
+    return render_template('register.html')
+
+@views.route('/login',methods=['GET','POST'])
 def login():
-    return '<h1>Login Page</h1>'
+    if request.method == 'POST':
+        username=request.form.get('username')
+        password=request.form.get('password')
+        user=Users.query.filter_by(username=username).first()
+        if user:
+            if check_password_hash(user.password,password):
+                login_user(user)
+                print("Logged In")
+                return redirect(url_for('views.posts'))
+
+            else:
+                print("wrong password")
+        else:
+            print("username does'nt exiest")
+    return render_template('login.html')
+
+
+@login_required
+@views.route('/posts')
+def posts():
+    return '<h1>Welcome to POST</h1>'
+
+
+@login_required
+@views.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('views.mainroute'))
